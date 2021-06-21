179328ab444bb9a9180560b49a0d6112264ddd60
유씨웨어개발팀 <seoyeongje@yussiweeogaebaltim-ui-MacBookPro.local>
2021-06-21 14:29:58 +0900

    add exection & write annotation & modify pool handle

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 /*eslint-disable */
 const _ = require("lodash");
-const { ServiceSchemaError } = require("moleculer").Errors;
+const { MoleculerServerError } = require("moleculer").Errors;
 const mariadb = require("mariadb");
 const mybatisMapper = require("mybatis-mapper");
 
-class MariaAdapter {
+class MariaDbAdapter {
+  /**
+   * Creates an instance of MariaDbAdapter.
+   * @param {mariadb.PoolConfig} opts
+   *
+   * @memberof MariaDbAdapter
+   */
   constructor(opts) {
     this.opts = opts;
     this.mapper = _.cloneDeep(mybatisMapper);
-    this.pool = mariadb.createPool(this.opts);
   }
+
   /**
    * Initialize adapter
    *
@@ -23,25 +29,56 @@ class MariaAdapter {
     this.service = service;
   }
 
+  /**
+   * Connect to database
+   *
+   * @returns {Promise}
+   *
+   * @memberof MariaDbAdapter
+   */
   connect() {
+    if (!this.service.schema.settings.mapperDir) {
+      throw new MoleculerServerError(
+        "Missing `mapperDir` definition in schema.settings of service!"
+      );
+    }
+    this.pool = mariadb.createPool(this.opts);
     this.mapper.createMapper([this.service.schema.settings.mapperDir]);
     return this.pool.getConnection().then((conn) => {
       this.db = conn;
-      this.db.queryExecute = this.queryExecute.bind(this);
+      this.db.sendQuery = this.sendQuery.bind(this);
       this.service.logger.info("MariaDB adapter has connected successfully.");
     });
   }
 
+  /**
+   * Disconnect from database
+   *
+   * @returns {Promise}
+   *
+   * @memberof MariaDbAdapter
+   */
   disconnect() {
-    return this.pool.end();
+    if (this.pool) {
+      this.pool.end();
+    }
+    return Promise.resolve();
   }
 
-  queryExecute(id, params) {
-    console.log(this.mapper);
+  /**
+   * Send SQL Query to Database
+   *
+   * @param {string} id
+   * @param {object?} params
+   * @returns {Promise}
+   *
+   * @memberof MariaDbAdapter
+   */
+  sendQuery(id, params) {
     const sql = this.mapper.getStatement("mariadb", id, params);
     this.service.logger.info(sql);
     return this.db.query(sql);
   }
 }
 
-module.exports = MariaAdapter;
+module.exports = MariaDbAdapter;
